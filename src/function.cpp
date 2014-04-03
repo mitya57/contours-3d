80106015bae6099c35f301db72b1b39236c25b18
Dmitry Shachnev <mitya57@gmail.com>
2014-04-03 21:00:59 +0400

    Initial commit

---
+++ src/function.cpp
@@ -0,0 +1,74 @@
+#include "struct.h"
+
+template <class CoordType>
+void processPoint(Point3D<CoordType>     point,
+                  VoxelArray<CoordType> *array,
+                  Vector3D<unsigned>     index,
+                  CoordType             *currentLength)
+{
+    if (!array[index] || !array->verifyIndex(index)) {
+        return;
+    }
+    CoordType length = (point - array->getVoxelCenter(index)).length();
+    if (*currentLength < 0 || length < *currentLength) {
+        *currentLength = currentLength;
+    }
+}
+
+template <class CoordType>
+CoordType getDistance(Point3D<CoordType>     point,
+                      VoxelArray<CoordType> *array)
+{
+    CoordType currentLength = -1;
+    int x, y, z;
+
+    Vector3D<unsigned> initialIndex(
+        (point.x - array->start.x) / array->voxelSize.x,
+        (point.y - array->start.y) / array->voxelSize.y,
+        (point.z - array->start.z) / array->voxelSize.z
+    );
+    if (*array[initialIndex]) {
+        return 0;
+    }
+
+    for (unsigned layerInd = 1;; ++layerInd) {
+        /* min z and max z */
+        for (x = initialIndex.x - layerInd; x <= initialIndex.x + layerInd; ++x) {
+            for (y = initialIndex.y - layerInd; y <= initialIndex.y + layerInd; ++y) {
+                processPoint(point, array,
+                             Vector3D<unsigned>(x, y, initialIndex.z - layerInd),
+                             &currentLength);
+                processPoint(point, array,
+                             Vector3D<unsigned>(x, y, initialIndex.z + layerInd),
+                             &currentLength);
+            }
+        }
+        /* min y and max y */
+        for (x = initialIndex.x - layerInd; x <= initialIndex.x + layerInd; ++x) {
+            for (z = initialIndex.z - layerInd + 1; z < initialIndex.z + layerInd; ++z) {
+                processPoint(point, array,
+                             Vector3D<unsigned>(x, initialIndex.y - layerInd, z),
+                             &currentLength);
+                processPoint(point, array,
+                             Vector3D<unsigned>(x, initialIndex.y + layerInd, z),
+                             &currentLength);
+            }
+        }
+        /* min x and max x */
+        for (y = initialIndex.y - layerInd + 1; y < initialIndex.y + layerInd; ++y) {
+            for (z = initialIndex.z - layerInd + 1; z < initialIndex.z + layerInd; ++z) {
+                processPoint(point, array,
+                             Vector3D<unsigned>(initialIndex.x - layerInd, y, z),
+                             &currentLength);
+                processPoint(point, array,
+                             Vector3D<unsigned>(initialIndex.x + layerInd, y, z),
+                             &currentLength);
+            }
+        }
+        if (currentLength > 0) {
+            return currentLength;
+        }
+    }
+
+    return currentLength;
+}
---
+++ src/struct.h
@@ -0,0 +1,96 @@
+#ifndef _STRUCT_H
+#define _STRUCT_H
+#include <cmath>
+
+template <class CoordType=float> struct Point3D;
+template <class CoordType=float> struct Vector3D;
+template <class CoordType=float> struct Box;
+template <class CoordType=float> struct VoxelArray;
+
+template <class CoordType>
+struct Point3D {
+    CoordType x;
+    CoordType y;
+    CoordType z;
+
+    Point3D<CoordType>(CoordType x, CoordType y, CoordType z):
+        x(x), y(y), z(z) {}
+
+    inline Point3D<CoordType> operator + (Vector3D<CoordType> &that) const {
+        return Point3D<CoordType>(x + that.x, y + that.y, z + that.z);
+    }
+    inline Point3D<CoordType> operator * (CoordType factor) const {
+        return Point3D<CoordType>(x * factor, y * factor, z * factor);
+    }
+    inline Point3D<CoordType> operator / (CoordType factor) const {
+        return Point3D<CoordType>(x / factor, y / factor, z / factor);
+    }
+    inline Vector3D<CoordType> operator - (Point3D<CoordType> &that) const {
+        return Vector3D<CoordType>(x - that.x, y - that.y, z - that.z);
+    }
+};
+
+template <class CoordType>
+struct Vector3D {
+    CoordType x;
+    CoordType y;
+    CoordType z;
+
+    Vector3D<CoordType>(CoordType x, CoordType y, CoordType z):
+        x(x), y(y), z(z) {}
+
+    inline CoordType length() const {
+        return sqrt(x * x + y * y + z * z);
+    }
+    inline Vector3D<CoordType> operator * (Vector3D<unsigned> &factors) const {
+        return Vector3D<CoordType>(x * factors.x, y * factors.y, z * factors.z);
+    }
+    inline Vector3D<CoordType> &operator = (Vector3D<CoordType> &that) {
+        x = that.x;
+        y = that.y;
+        z = that.z;
+        return *this;
+    }
+    inline Vector3D<CoordType> operator + (Vector3D<CoordType> &that) const {
+        return Vector3D<CoordType>(x + that.x, y + that.y, z + that.z);
+    }
+};
+
+template <class CoordType>
+struct Box {
+    Point3D<CoordType> min;
+    Point3D<CoordType> max;
+
+    Box<CoordType>(Point3D<CoordType> min, Point3D<CoordType> max):
+        min(min), max(max) {}
+
+    inline bool contains(Point3D<CoordType> point) const {
+        return (point.x > min.x && point.y > min.y && point.z > min.z &&
+                point.x < max.x && point.y < max.y && point.z < max.z);
+    }
+};
+
+template <class CoordType>
+struct VoxelArray {
+    bool *voxels;
+    Point3D<CoordType> start;
+    Vector3D<CoordType> voxelSize;
+    Vector3D<unsigned> size;
+
+    inline Box<CoordType> getBox() const {
+        return Box<CoordType>(start, start + voxelSize * size);
+    }
+    inline bool verifyIndex(Vector3D<unsigned> index) const {
+        return (index.x > 0 && index.x < size.x &&
+                index.y > 0 && index.y < size.y &&
+                index.z > 0 && index.z < size.z);
+    }
+    inline bool &operator [] (Vector3D<unsigned> index) {
+        return voxels[index.z * size.x * size.y + index.y * size.x + index.z];
+    }
+    inline CoordType getVoxelCenter (Vector3D<unsigned> index) const {
+        return start + (voxelSize / 2) + (voxelSize * index);
+    }
+};
+
+#endif /* _STRUCT_H */
