d4dab5f92499c6871c4f21bd3da7987337c90bfd
Dmitry Shachnev <mitya57@gmail.com>
2014-05-05 11:07:09 +0400

    gui: Add MainWindow

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,9 +1,24 @@
+#include <QtGui/QIcon>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QSlider>
+#include <QtWidgets/QMainWindow>
 #include "triangview.h"
 
 int main(int argc, char **argv) {
     QApplication app(argc, argv);
+    QMainWindow window;
     TriangView view;
-    view.show();
+    QToolBar toolBar;
+    QSlider slider(Qt::Horizontal);
+
+    toolBar.addWidget(&slider);
+    window.resize(view.size());
+    window.setWindowIcon(QIcon::fromTheme("application-x-executable"));
+    window.setWindowTitle("3D interpolation");
+    window.setCentralWidget(&view);
+ // window.addToolBar(Qt::BottomToolBarArea, &toolBar);
+    window.show();
+
     return app.exec();
 }
