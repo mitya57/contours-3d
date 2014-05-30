fcbc4317da79bb6a56acde08a5e6a2cf04138c56
Dmitry Shachnev <mitya57@gmail.com>
2014-05-30 14:00:38 +0400

    Make the code compile

--- gui/triangview.pro
+++ gui/triangview.pro
@@ -4,5 +4,6 @@ CONFIG += debug
 QT += opengl
 
 SOURCES += triangview.cpp main.cpp
+SOURCES += ../src/triang.cpp
 HEADERS += triangview.h
 INCLUDEPATH += ../src
--- src/triang.cpp
+++ src/triang.cpp
@@ -6,16 +6,12 @@ Index3D getVertexOffset(char vnumber) {
         vnumber & 1,
         bool(vnumber & 2),
         bool(vnumber & 4)
-    )
+    );
 }
 
-template <class CoordType, Point3D<CoordType> center, CoordType radius>
-CoordType sphereFunction(Point3D<CoordType> point) {
-    return radius - (point - center).length();
+float mySphereFunction(Point3Df point) {
+    return .4f - (point - Point3Df(.5f, .5f, .5f)).length();
 }
-
-float (*mySphereFunction) (Point3D<float>) =
-    sphereFunction<float, Point3Df(.5f, .5f, .5f), .4f>
 
 void getTetrahedByIndex(char index, char *tnumbers) {
     tnumbers[0] = 0;
@@ -31,10 +27,10 @@ struct IndexPair3D {
     IndexPair3D(Index3D _i1, Index3D _i2):
         i1(_i1), i2(_i2) {}
 
-    bool operator == (IndexPair3D const &that) {
+    bool operator == (IndexPair3D const &that) const {
         return i1 == that.i1 && i2 == that.i2;
     }
-}
+};
 
 template <class Type>
 int findInVector(std::vector<Type> const &vect, Type const &element) {
@@ -52,14 +48,15 @@ int appendToVector(std::vector<Type> &vect, Type const &element) {
     if (found >= 0) {
         return found;
     }
-    vector.push_back(element);
-    return vector.size() - 1;
+    vect.push_back(element);
+    return vect.size() - 1;
 }
 
-int addIndexPair(std::vector<IndexPair3D> &, Type const) = appendToVector<IndexPair3D>;
+int (*addIndexPair) (std::vector<IndexPair3D> &, IndexPair3D const &) =
+appendToVector<IndexPair3D>;
 
-int fillVectors(std::vector<Point3Df> points;
-                std::vector<Triangle3Df> triangles) {
+void fillVectors(std::vector<Point3Df> points,
+                 std::vector<Triangle3Df> triangles) {
     const unsigned size = 20;
     const float rsize = .05f;
 
@@ -92,7 +89,7 @@ int fillVectors(std::vector<Point3Df> points;
         for (j = 0; j < 6; ++j) {
             getTetrahedByIndex(j, tnumbers);
             for (k = 0; k < 4; ++k) {
-                fvalues[k] = distArray[ind3d + getVertexOffset(tnumbers[k])];
+                fvalues[k] = distArray[voxelArray.num(ind3d + getVertexOffset(tnumbers[k]))];
             }
             negative = 0;
             for (k = 0; k < 4; ++k) {
@@ -109,35 +106,43 @@ int fillVectors(std::vector<Point3Df> points;
             /* FIXME: set normals correctly */
             if (negative == 1) {
                 /* Triangle(01, 02, 03) */
-                triangles.push_back(
-                    addIndexPair(indexPairs, IndexPair3D(tnumbers[0], tnumbers[1])),
-                    addIndexPair(indexPairs, IndexPair3D(tnumbers[0], tnumbers[2])),
-                    addIndexPair(indexPairs, IndexPair3D(tnumbers[0], tnumbers[3]))
-                );
+                triangles.push_back(Triangle3Df(
+                    addIndexPair(indexPairs, IndexPair3D(ind3d + getVertexOffset(tnumbers[0]),
+                                                         ind3d + getVertexOffset(tnumbers[1]))),
+                    addIndexPair(indexPairs, IndexPair3D(ind3d + getVertexOffset(tnumbers[0]),
+                                                         ind3d + getVertexOffset(tnumbers[2]))),
+                    addIndexPair(indexPairs, IndexPair3D(ind3d + getVertexOffset(tnumbers[0]),
+                                                         ind3d + getVertexOffset(tnumbers[3])))
+                ));
             } else if (negative == 2) {
                 /* Rectangle(02, 12, 13), Rectangle(02, 03, 13) */
-                triangles.push_back(
-                    addIndexPair(indexPairs, IndexPair3D(tnumbers[0], tnumbers[2])),
-                    addIndexPair(indexPairs, IndexPair3D(tnumbers[1], tnumbers[2])),
-                    addIndexPair(indexPairs, IndexPair3D(tnumbers[1], tnumbers[3]))
-                );
-                triangles.push_back(
-                    addIndexPair(indexPairs, IndexPair3D(tnumbers[0], tnumbers[2])),
-                    addIndexPair(indexPairs, IndexPair3D(tnumbers[0], tnumbers[3])),
-                    addIndexPair(indexPairs, IndexPair3D(tnumbers[1], tnumbers[3]))
-                );
+                triangles.push_back(Triangle3Df(
+                    addIndexPair(indexPairs, IndexPair3D(ind3d + getVertexOffset(tnumbers[0]),
+                                                         ind3d + getVertexOffset(tnumbers[2]))),
+                    addIndexPair(indexPairs, IndexPair3D(ind3d + getVertexOffset(tnumbers[1]),
+                                                         ind3d + getVertexOffset(tnumbers[2]))),
+                    addIndexPair(indexPairs, IndexPair3D(ind3d + getVertexOffset(tnumbers[1]),
+                                                         ind3d + getVertexOffset(tnumbers[3])))
+                ));
+                triangles.push_back(Triangle3Df(
+                    addIndexPair(indexPairs, IndexPair3D(ind3d + getVertexOffset(tnumbers[0]),
+                                                         ind3d + getVertexOffset(tnumbers[2]))),
+                    addIndexPair(indexPairs, IndexPair3D(ind3d + getVertexOffset(tnumbers[0]),
+                                                         ind3d + getVertexOffset(tnumbers[3]))),
+                    addIndexPair(indexPairs, IndexPair3D(ind3d + getVertexOffset(tnumbers[1]),
+                                                         ind3d + getVertexOffset(tnumbers[3])))
+                ));
             }
         }
     }
 
-    IndexPair3D &pair;
-    for (i = 0; i < indexPairs.size(); ++i) {
-        pair = indexPairs[i];
-        float val1 = distArray[ind3d + getVertexOffset(pair.i1)];
-        float val2 = distArray[ind3d + getVertexOffset(pair.i2)];
-        Point3Df p1 = Point3Df(rsize, rsize, rsize) * pair.i1;
-        Point3Df p2 = Point3Df(rsize, rsize, rsize) * pair.i2;
-        points.push_back((p1 * val2 - p2 * val1) / (val2 - val1));
+    std::vector<IndexPair3D>::const_iterator it;
+    for (it = indexPairs.begin(); it != indexPairs.end(); ++it) {
+        float val1 = distArray[voxelArray.num(it->i1)];
+        float val2 = distArray[voxelArray.num(it->i2)];
+        Point3Df p1 = Point3Df(rsize, rsize, rsize) * it->i1;
+        Point3Df p2 = Point3Df(rsize, rsize, rsize) * it->i2;
+        points.push_back((p1 * val2 + p2 * (-val1)) / (val2 - val1));
     }
 
     delete[] distArray;
